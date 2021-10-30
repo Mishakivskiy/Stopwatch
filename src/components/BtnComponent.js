be59e47238b2939d2af1fc5e05cc3f19420342e7
Mishakivskiy <91463513+Mishakivskiy@users.noreply.github.com>
2021-10-30 13:05:23 +0300

    1st commit

---
+++ src/components/BtnComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function BtnComponent(props) {
+  return (
+    <div>
+      {(props.status === 0)?
+        <button className="stopwatch-btn stopwatch-btn-gre"
+        onClick={props.start}>Start</button> : ""
+      }
+      {(props.status === 1)?
+       <div>
+          <button className="stopwatch-btn stopwatch-btn-red"
+          onClick={props.reset}>Stop</button> 
+
+          <button className="stopwatch-btn stopwatch-btn-yel"
+          onDoubleClick={props.wait}>Wait</button> 
+        </div> : ""
+      }
+
+      {(props.status === 2)?
+       <div>
+          <button className="stopwatch-btn stopwatch-btn-gre"
+          onClick={props.resume}>Start</button> 
+
+          <button className="stopwatch-btn stopwatch-btn-yel"
+          onClick={props.reset}>Reset</button> 
+        </div> : ""
+      }
+
+    </div>
+  );
+}
+
+export default BtnComponent;
